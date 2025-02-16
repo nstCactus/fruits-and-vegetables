44a88ecb7fef3b908f15d617bd590d7caf6adb9e
nstcactus <nstcactus@gmail.com>
2025-02-16 15:13:43 +0100

    feat: add rector

---
+++ rector.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Symfony\Set\SymfonySetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/migrations',
+    ])
+
+    ->withSymfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml')
+    ->withSets([
+        SymfonySetList::SYMFONY_64,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+    ])
+    ->withPhpSets()
+    ->withAttributesSets()
+    ->withTypeCoverageLevel(2)
+    ->withDeadCodeLevel(2)
+    ->withCodeQualityLevel(2);
